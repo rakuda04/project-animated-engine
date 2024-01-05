1f1eaa360914294a632e251763f9f372e797b95e
rakuda04 <abdulkaderbaghaffar@gmail.com>
2024-01-05 17:57:11 +0300

    added animation to html

--- moushikomi.js
+++ moushikomi.js
@@ -1,4 +1,15 @@
-const validation = document.querySelector('.cta-button');
+
+var elements = document.querySelectorAll('.animate');
+var animations = ['animateTop','animateLeft', 'animateRight', 'animateLeft', 'animateRight', 'animateBottom']; //t,l,r,l,r,d
+
+elements.forEach(function(element, index) {
+    setTimeout(function() {
+        element.classList.remove('hidden');
+        element.classList.add(animations[index]);
+    }, index * 1000); // 1000ms delay between each animation
+});
+
+validation = document.querySelector('.cta-button');
 addEventListener('click', (e) => {
 
 })
